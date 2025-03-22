8ed515775a4a017734708a0cb90c7f4a3b076ffb
Suthishma <suthishmababu@gmail.com>
2025-03-22 16:24:08 +0530

    Updated project with new changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ToDo-App/', // Ensure the repository name is correct and has slashes
+  base: '/ToDo-App/', // Ensure correct repo name
+ 
 });
